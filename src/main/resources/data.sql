08898debf2e94d67755f5cb9f7aed05c2705c147
e.utkin <e.utkin@bi.zone>
2022-08-16 14:49:10 +0300

    -

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into authors(name)
-values ('A.С. Пушкин');
-insert into books(name, author_id)
-values ('Сказки', 1);
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into authors(name) values ('A.С. Пушкин');
+insert into books(name, author_id) values ('Сказки', 1);
